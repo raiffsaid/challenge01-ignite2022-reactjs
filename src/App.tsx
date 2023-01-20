783db6be02fd9cdf7453e59ba3fcbdea50ecd04a
Raiff Said <raiff.said@gmail.com>
2023-01-20 16:28:38 -0300

    feat: Input behaviour and state

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,22 @@ import './global.css';
 import styles from './App.module.css';
 import { Header } from './Header';
 import { InputArea } from './InputArea';
+import { TaskArea } from './TaskArea';
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState(0);
+  const [finishedTasks, setFinishedTasks] = useState(0);
+
   return (
     <>
       <Header />
 
       <main>
         <div className={styles.container}>
-          <InputArea />
+          <InputArea tasks={tasks} setTasks={setTasks} />
 
-          <div className={styles.listContainer}>
-            <span>Tarefas criadas</span>
-            <span>Concluídas</span>
-          </div>
-          <div></div>
+          <TaskArea tasks={tasks} />
         </div>
       </main>
     </>
--- src/InputArea.tsx
+++ src/InputArea.tsx
@@ -1,12 +1,34 @@
 import styles from './InputArea.module.css';
 import buttonImg from './assets/plus.svg';
+import { ChangeEvent, FormEvent, InputHTMLAttributes, useState } from 'react';
 
-export function InputArea() {
+interface InputAreaProps {
+  tasks: number;
+  setTasks(task: number): void;
+}
+
+export function InputArea({ tasks, setTasks }: InputAreaProps) {
+  
+  const [titleText, setTitleText] = useState('');
+
+  const isInputAreaEmpty = titleText.length === 0;
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    alert(`Tarefa ${titleText} criada`);
+    setTasks(tasks + 1);
+    setTitleText('');
+  }
+
+  function handleTitleTextChange(event: ChangeEvent<HTMLInputElement>) {
+    setTitleText(event.target.value);
+  }
+
   return (
     <div className={styles.wrapper}>
-      <form action="">
-        <input type="text" placeholder="Adicione uma nova tarefa"/>
-        <button type="submit">
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Adicione uma nova tarefa" value={titleText} onChange={handleTitleTextChange}/>
+        <button type="submit" disabled={isInputAreaEmpty}>
           Criar <span><img src={buttonImg}/></span>
         </button>
       </form>
